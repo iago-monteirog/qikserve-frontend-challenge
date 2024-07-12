02f01a5a9c39f3475fc1360d8f87ea57954fb27c
iago-monteirog <iagomgarcia9000@gmail.com>
2024-07-11 23:15:04 -0300

    fix: add headter to get requisitions

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -3,13 +3,18 @@ import api from "../lib/axios";
 
 export const fetchAndParseData = async <T>(url: string, schema: ZodSchema<T>): Promise<T> => {
     try {
-        const response = await api.get(url)
-        
+        const response = await api.get(url, {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+
         const data = response.data
 
         return schema.parse(data)
     } catch (error) {
         console.error(`Error fetching data from ${url}`, error);
-        throw new Error(`Error fetching data from ${url}`); 
+        throw new Error(`Error fetching data from ${url}`);
     }
 }
