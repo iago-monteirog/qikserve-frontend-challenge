00105ce42cb7f3fa3c8798f0f77f029abf6b4beb
iago-monteirog <iagomgarcia9000@gmail.com>
2024-07-12 07:32:43 -0300

    build: add vercel config json

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
-  // Carregar variáveis de ambiente com base no `mode`
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -34,7 +33,7 @@ export default defineConfig(({ command, mode }) => {
     },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
-      'process.env': env, // Esta linha permite que você use `process.env` no seu código
+      'process.env': env, 
     },
   };
 });
